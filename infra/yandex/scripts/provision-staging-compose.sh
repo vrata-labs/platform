c3737c4768bcb47368b2fea2c15f0ca77b9700b5
Serkov Dmitry (gpt 4.5 preview) <you@example.com>
2026-03-30 22:37:02 +0300

    Enable SSH access for compose staging VMs

--- infra/yandex/scripts/provision-staging-compose.sh
+++ infra/yandex/scripts/provision-staging-compose.sh
@@ -9,13 +9,24 @@ MEMORY="4GB"
 IMAGE_FAMILY="ubuntu-2204-lts"
 IMAGE_FOLDER="standard-images"
 USER_DATA_FILE="infra/yandex/cloud-init/staging-compose.yaml"
+SSH_KEY_FILE="${YC_SSH_KEY_FILE:-$HOME/.ssh/id_ed25519.pub}"
+SSH_LOGIN="${YC_SSH_LOGIN:-$USER}"
 
-yc compute instance create \
-  --name "$INSTANCE_NAME" \
-  --zone "$ZONE" \
-  --platform-id "$PLATFORM_ID" \
-  --create-boot-disk image-folder-id="$IMAGE_FOLDER",image-family="$IMAGE_FAMILY",size=20GB \
-  --cores "$CORES" \
-  --memory "$MEMORY" \
-  --network-interface subnet-name=default-"$ZONE",nat-ip-version=ipv4 \
+YC_ARGS=(
+  compute instance create
+  --name "$INSTANCE_NAME"
+  --zone "$ZONE"
+  --platform-id "$PLATFORM_ID"
+  --create-boot-disk image-folder-id="$IMAGE_FOLDER",image-family="$IMAGE_FAMILY",size=20GB
+  --cores "$CORES"
+  --memory "$MEMORY"
+  --network-interface subnet-name=default-"$ZONE",nat-ip-version=ipv4
   --metadata-from-file user-data="$USER_DATA_FILE"
+)
+
+if [ -f "$SSH_KEY_FILE" ]; then
+  SSH_KEY_CONTENT="$(tr -d '\n' < "$SSH_KEY_FILE")"
+  YC_ARGS+=(--metadata "ssh-keys=${SSH_LOGIN}:${SSH_KEY_CONTENT}")
+fi
+
+yc "${YC_ARGS[@]}"
